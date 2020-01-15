189ac8aaa8d6232c0f2b8e1c0230ec594c06b654
marciodias <marciodias@marciodiass-iMac.local>
2020-01-15 13:01:23 -0300

    Fix

--- web/src/App.js
+++ web/src/App.js
@@ -7,7 +7,34 @@ function App() {
 
   return (
     <div id="app">
-      <aside></aside>
+      <aside>
+        <strong className="">Cadastrar</strong>
+        <form>
+
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div class="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
       <main></main>
     </div>
   );
