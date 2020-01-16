eccd0849376320b8df189c599618d2ad531b768e
marciodias <marciodias@marciodiass-iMac.local>
2020-01-16 18:50:57 -0300

    Fix

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
@@ -33,7 +33,16 @@ function Main() {
 
   return (
     <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -27.2111164, longitude: -49.6374491}} />
+      <Marker coordinate={{ latitude: -27.2111164, longitude: -49.6374491}}>
+        <Image styles={styles.avatar} source={{uri:'https://avatars1.githubusercontent.com/u/75541?s=460&v=4'}} />
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Diego Fernandes</Text>
+            <Text style={styles.devBio}>bio</Text>
+            <Text style={styles.devTechs}>Re4actJS, React Native, Node.js</Text>
+          </View>
+        </Callout>
+      </Marker>
     </MapView>
   );
 }
@@ -42,6 +51,27 @@ const styles = StyleSheet.create({
   map: {
     flex:1
   },
+  avatar: {
+    width:54,
+    height:54,
+    borderRadius:4,
+    borderWidth:4,
+    borderColor:'#FFF'
+  },
+  callout: {
+    width:260
+  },
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  devBio: {
+    color: '#666',
+    marginTop:5,
+  },
+  devTechs: {
+    marginTop: 5
+  }
 });
 
 export default Main;
