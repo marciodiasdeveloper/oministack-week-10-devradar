6e7d62141c5b8af4117e6be88c11f60dd7549dce
marciodias <marciodias@marciodiass-iMac.local>
2020-01-17 09:36:00 -0300

    Fix

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,7 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
-import {connect,disconnect} from '../services/socket';
+import {connect,disconnect,subscribeToNewDevs} from '../services/socket';
 
 function Main({navigation}) {
 
@@ -37,7 +37,12 @@ function Main({navigation}) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, devs]))
+  }, [devs])
+
   function setupWebsocket() {
+    disconnect();
     const {latitude,longitude} = currentRegion;
     connect({
       latitude,
@@ -56,6 +61,7 @@ function Main({navigation}) {
     console.log(response.data);
     setDevs(response.data.devs);
     setupWebsocket();
+    su
   }
 
   function handleRegionChange(region) {
--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -4,6 +4,10 @@ const socket = socketio('http://192.168.1.109:3333', {
   autoConnect: false
 });
 
+function subscribeToNewDevs(subscribeFunction) {
+  socket.on('new-dev', subscribeFunction);
+}
+
 function connect(latitude, longitude, techs) {
   
   socket.io.opts.query = {
